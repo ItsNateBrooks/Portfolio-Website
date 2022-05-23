10366f225d227159107ed0c451f57eec0fd9bd24
ItsNateBrooks <ItsNateBrooks@gmail.com>
2022-05-23 14:40:50 -0400

    File Restructure & zooming
    Files have been reorganized to be more uniform and for the first display it can be zoomed in on.

--- main.js
+++ main.js
@@ -3,14 +3,11 @@ import * as THREE from 'three';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
 import gsap from 'gsap';
 
-let state = 0;
-
+//setup scene, camera, and renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight, 0.1, 1000);
-camera.position.set(0,6.5,-6.5);
+camera.position.set(0,6.5,-8.8);
 camera.lookAt(0, 5, -12.8);
-
-
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg')
@@ -19,44 +16,38 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.render(scene, camera);
 
+//Setup light, light helper, and grid helper
 const pointLight = new THREE.PointLight(0xffffff);
 
 pointLight.position.set(25,25,25);
 scene.add(pointLight);
 
-
 const lightHelper = new THREE.PointLightHelper(pointLight);
 const gridHelper = new THREE.GridHelper(200,50);
-scene.add(lightHelper, gridHelper);
+scene.add(lightHelper);//, gridHelper
 
-const nateTexture = new THREE.TextureLoader().load('Nate.jpg');
-const nate = new THREE.Mesh(
-  new THREE.BoxGeometry(1.25, 1.25, 1.25),
-  new THREE.MeshBasicMaterial({map:nateTexture})
-);
-nate.position.set(0, 5, -12.8);
-scene.add(nate)
-
+//Camera Control
+let state = 0;
 window.addEventListener('wheel', function(event){
 
   switch (state){
     case 0:
-      if (!checkScrollDirectionIsUp(event)) {
+      if (!checkScrollDirectionIsUp(event) && !subState) {
         state = 1;
         scrollHelper(3,6.5,-8.25,7.5, 5, -8.25);
       }
     break
     case 1:
-      if (checkScrollDirectionIsUp(event)) {
+      if (checkScrollDirectionIsUp(event)&& !subState) {
         state = 0;
-        scrollHelper(0,6.5,-6.5,0, 5, -12.8);
+        scrollHelper(0,6.5,-8.8,0, 5, -12.8);
       }else{
         state = 2;
         scrollHelper(-3,6.5,-8.25,-7.5, 5, -8.25);
       }  
     break
     case 2:
-      if (checkScrollDirectionIsUp(event)) {
+      if (checkScrollDirectionIsUp(event)&& !subState) {
         state = 1;
         scrollHelper(3,6.5,-8.25,7.5, 5, -8.25);
       }else{
@@ -65,7 +56,7 @@ window.addEventListener('wheel', function(event){
       }  
     break
     case 3:
-      if (checkScrollDirectionIsUp(event)) {
+      if (checkScrollDirectionIsUp(event)&& !subState) {
         state = 2;
         scrollHelper(-3,6.5,-8.25,-7.5, 5, -8.25);
       }else{
@@ -74,7 +65,7 @@ window.addEventListener('wheel', function(event){
       }
     break
     case 4:
-      if (checkScrollDirectionIsUp(event)) {
+      if (checkScrollDirectionIsUp(event)&& !subState) {
         state = 3;
         scrollHelper(3,6.5,-1.95,7.5, 5, -1.95);
       }
@@ -83,6 +74,57 @@ window.addEventListener('wheel', function(event){
   
 });
 
+let subState = false;
+window.addEventListener('mousedown', function(event){
+
+  switch (state){
+    case 0:
+      if (subState) {
+        subState = false;
+        scrollHelper(0,6.5,-8.8,0, 5, -12.8);
+      }else{
+        subState = true;
+        scrollHelper(1.5, 5, -11.8,1.5, 5, -12.8);
+      }
+    break/*
+    case 1:
+      if (subState) {
+        state = 0;
+        scrollHelper(0,6.5,-8.8,0, 5, -12.8);
+      }else{
+        state = 2;
+        scrollHelper(-3,6.5,-8.25,-7.5, 5, -8.25);
+      }  
+    break
+    case 2:
+      if (subState) {
+        state = 1;
+        scrollHelper(3,6.5,-8.25,7.5, 5, -8.25);
+      }else{
+        state = 3;
+        scrollHelper(3,6.5,-1.95,7.5, 5, -1.95);
+      }  
+    break
+    case 3:
+      if (subState) {
+        state = 2;
+        scrollHelper(-3,6.5,-8.25,-7.5, 5, -8.25);
+      }else{
+        state = 4;
+        scrollHelper(-3, 6.5, -1.95,-7.5, 5, -1.95);
+      }
+    break
+    case 4:
+      if (subState) {
+        state = 3;
+        scrollHelper(3,6.5,-1.95,7.5, 5, -1.95);
+      }
+    break*/
+    }
+  
+});
+
+// Helper function for Camera Controls
 function scrollHelper(pox,poy,poz,lax,lay,laz) {
   gsap.to(camera.position, {
     x: pox,
@@ -95,7 +137,7 @@ function scrollHelper(pox,poy,poz,lax,lay,laz) {
   });
 }
 
-
+// Helper function to check if user is scrolling up or down with mouse wheel
 function checkScrollDirectionIsUp(event) {
   if (event.wheelDelta) {
     return event.wheelDelta > 0;
@@ -103,8 +145,27 @@ function checkScrollDirectionIsUp(event) {
   return event.deltaY < 0;
 }
 
+//Nate Object (object with my face on it) loading
+const nateTexture = new THREE.TextureLoader().load('Nate.jpg');
+const nate = new THREE.Mesh(
+  new THREE.BoxGeometry(1.25, 1.25, 1.25),
+  new THREE.MeshBasicMaterial({map:nateTexture})
+);
+nate.position.set(0, 5, -12.8);
+scene.add(nate)
+
+//Text loading
+
+//Adds skybox
+let skyboxImage = "bluecloud";
+const materialArray = createMaterialArray(skyboxImage);
+const skyboxGeo = new THREE.BoxGeometry(1000, 1000, 1000);
+const skybox = new THREE.Mesh(skyboxGeo, materialArray);
+scene.add(skybox);
+
+//Skybox helper to load sky assets
 function createPathStrings(filename) {
-  const basePath = "Clouds/";
+  const basePath = "Assets/clouds/";
   const baseFilename = basePath + filename;
   const fileType = ".jpg";
   const sides = ["ft", "bk", "up", "dn", "rt", "lf"];
@@ -114,6 +175,7 @@ function createPathStrings(filename) {
   return pathStings;
 }
 
+//Skybox helper to create geometry
 function createMaterialArray(filename) {
   const skyboxImagepaths = createPathStrings(filename);
   const materialArray = skyboxImagepaths.map(image => {
@@ -123,14 +185,10 @@ function createMaterialArray(filename) {
   return materialArray;
 }
 
-let skyboxImage = "bluecloud";
-const materialArray = createMaterialArray(skyboxImage);
-const skyboxGeo = new THREE.BoxGeometry(1000, 1000, 1000);
-const skybox = new THREE.Mesh(skyboxGeo, materialArray);
-scene.add(skybox);
 
-const loader = new GLTFLoader()
-loader.load('Assets/scene.gltf', function(gltf){
+//Loads room 3d model
+const roomloader = new GLTFLoader()
+roomloader.load('Assets/models/building/scene.gltf', function(gltf){
   console.log(gltf)
   const root = gltf.scene;
   root.castShadow = true;
@@ -143,14 +201,40 @@ loader.load('Assets/scene.gltf', function(gltf){
   console.log("An error occurred")
 })
 
+//frame 3d model
+//======================================
+const frameloader = new GLTFLoader()
+frameloader.load('Assets/models/frame/scene.gltf', function(gltf){
+  console.log(gltf)
+  const root = gltf.scene;
+  root.castShadow = true;
+  root.receiveShadow = false;
+  root.position.set(1.5, 5, -12.8);
+  root.rotation.z = 51.8355;
+  root.scale.set(0.5,0.5,0.5)
+  scene.add(root)
+}, function(xhr){
+  console.log((xhr.loaded/xhr.total * 100) + + "% loaded")
+}, function(error){
+  console.log("An error occurred")
+})
 
+//first myInfo Object (object with my name and contact info) loading 
+const myInfoTexture = new THREE.TextureLoader().load('Assets/myInfo/myInfo1.PNG');
+const myInfo = new THREE.Mesh(
+  new THREE.BoxGeometry(1.25, 1.25, 1.25),
+  new THREE.MeshBasicMaterial({map:myInfoTexture})
+);
+myInfo.scale.set(0.8,0.6 ,0)
+myInfo.position.set(1.5, 5, -12.8);
+scene.add(myInfo)
+
+//Animate or Loop function
 function animate(){
   requestAnimationFrame(animate);
 
   nate.rotation.y += 0.005;
   skybox.rotation.y += 0.0001;
-
-  
 
   renderer.render(scene,camera);
 }
